85382bd70c856c9ed6ef1a08ca5a4bc1ed583ef4
Resham <resha@MacBook-Air-2.local>
2025-11-30 22:17:28 -0800

    Milestone 4

--- 210-lab-36-starter.cpp
+++ 210-lab-36-starter.cpp
@@ -4,16 +4,21 @@
 #include <string>
 using namespace std;
 
+// Comment #1: Function prototypes.
 void printMenu();
 int getChoice();
 
 int main()
 {
+    // Comment #2: Open file and read strings into the tree.
     ifstream file;
     file.open("codes.txt");
     string code;
+    
+    // Comment #3: Create the binary tree object.
     StringBinaryTree tree;
 
+    // Comment #4: Read strings from the file and insert them into the BST tree.
     while (getline(file, code))
     {
         tree.insertNode(code);
@@ -31,22 +36,23 @@ int main()
 
         switch (choice)
         {
+            // Comment #5: Handle user choices.
             case 1:
                 cout << "Please enter a string to insert: ";
                 cin >> insertStr;
-                tree.insertNode(insertStr);
+                tree.insertNode(insertStr); // Comment #6: Insert the string into the tree.
 
                 break;
             case 2:
                 cout << "Please enter a string to remove: ";
                 cin >> insertStr;
-                tree.remove(insertStr);
+                tree.remove(insertStr); // Comment #7: Remove the string from the tree.
 
                 break;
             case 3:
                 cout << "Please enter a string to search for: ";
                 cin >> insertStr;
-                if (tree.searchNode(insertStr))
+                if (tree.searchNode(insertStr)) // Comment #8: Search for the string in the tree.
                 {
                     cout << insertStr << " was found in the tree." << endl;
                 }
@@ -61,6 +67,7 @@ int main()
                 cin >> insertStr;
                 if (tree.searchNode(insertStr))
                 {
+                    // Comment #9: Modify the string by removing and re-inserting it.
                     tree.remove(insertStr);
                     cout << "Enter the new string: ";
                     cin >> insertStr;
@@ -89,9 +96,10 @@ int main()
     return 0;
 }
 
+// Comment #10: Function to print the menu options.
 void printMenu()
 {
-    cout << "Menu Options:" << endl;
+    cout << endl << "Menu Options:" << endl;
     cout << "1. Insert a string" << endl;
     cout << "2. Remove a string" << endl;
     cout << "3. Search for a string" << endl;
@@ -102,6 +110,7 @@ void printMenu()
     cout << "8. Exit" << endl;
 }
 
+// Comment #11: Function to get the user's menu choice.
 int getChoice()
 {
     int choice;
