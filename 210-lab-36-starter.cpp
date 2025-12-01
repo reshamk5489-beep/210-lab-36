f2744fb0f929faa70528a734cdb46746fd8b73a0
Resham <resha@MacBook-Air-2.local>
2025-11-30 21:58:00 -0800

    Adding tree menu

--- 210-lab-36-starter.cpp
+++ 210-lab-36-starter.cpp
@@ -4,23 +4,75 @@
 #include <string>
 using namespace std;
 
+void printMenu();
+int getChoice();
+
 int main()
 {
-   ifstream file;
-   file.open("codes.txt");
-   string code;
-   StringBinaryTree tree;
+    ifstream file;
+    file.open("codes.txt");
+    string code;
+    StringBinaryTree tree;
 
-   while (getline(file, code))
-   {
-       tree.insertNode(code);
-   }
+    while (getline(file, code))
+    {
+        tree.insertNode(code);
+    }
 
-   file.close();
+    file.close();
 
-   tree.displayInOrder();
+    bool isExit = false;
+
+    while (isExit == false)
+    {
+        printMenu();
+        int choice = getChoice();
+
+        switch (choice)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                tree.displayInOrder();
+                break;
+            case 6:
+                tree.displayPreOrder();
+                break;
+            case 7:
+                tree.displayPostOrder();
+                break;
+            case 8:
+                return 0;
+        }
+    }
   
-   return 0;
+    return 0;
 }
 
-// Milestone 3
+void printMenu()
+{
+    cout << "Menu Options:" << endl;
+    cout << "1. Insert a string" << endl;
+    cout << "2. Remove a string" << endl;
+    cout << "3. Search for a string" << endl;
+    cout << "4. Modify a string" << endl;
+    cout << "5. Display In-Order" << endl;
+    cout << "6. Display Pre-Order" << endl;
+    cout << "7. Display Post-Order" << endl;
+    cout << "8. Exit" << endl;
+}
+
+int getChoice()
+{
+    int choice;
+    cout << endl << "Enter your choice: ";
+    cin >> choice;
+
+    return choice;
+}
